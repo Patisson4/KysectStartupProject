f8cff952e4df48b524079ee5714815ba8dd32fc7
Patisson4 <81909511+Patisson4@users.noreply.github.com>
2021-05-02 19:40:14 +0300

    Add files via upload

---
+++ ConsoleApp1/Task.cs
@@ -0,0 +1,26 @@
+﻿namespace StartingTaskManagerProject
+{
+    public class Task : BaseTask
+    {
+        public string TaskInfo { get; } //public for json
+
+        public Task(string taskInfo, bool isCompleted = false) : base(isCompleted)
+        {
+            TaskInfo = taskInfo;
+        }
+
+        public override string ToString()
+        {
+            return "Type: " + GetType().Name + "; Info: " + TaskInfo + "; Status: " +
+                   (IsCompleted ? "done" : "unsolved");
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            var task = (Task) obj;
+            return TaskInfo == task.TaskInfo;
+        }
+    }
+}
